1d6379003e4702f9e37755342386fcab0e6d94d4
Akhilesh Joshi <theakhileshjoshi@gmail.com>
2021-01-09 21:55:18 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 first_name = input("Enter first name.")
 last_name = input("Enter last name.")
-full_name = last_name+" "+first_name
+reverse_name = last_name+" "+first_name
 
-print(full_name)
+print(reverse_name)
